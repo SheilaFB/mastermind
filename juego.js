faa0249e39febae0d96d5a087b1e10a60c6c1853
sheila <sheila.feijoo@iessanmamede.com>
2024-02-28 17:01:22 +0100

    Se añaden botones con los colores disponibles. Se añaden los botones del intento. Se puede pulsar un color disponible y pintar con el el botón del intento.

--- juego.js
+++ juego.js
@@ -5,19 +5,47 @@ console.log('NIVEL: ' + nivel);
 console.log('COLORES: ' + colores);
 
 var numIntento = 0;
-let arrayColores = new Map();
+let mapColores = new Map();
 let intentos = new Map();
 let combinacion = crearCombinacion();
-comprobarIntento([6,5,3,2]);
+var tiradaAciertos = [];
+var colorPintar=0;
+comprobarIntento([1,1,1,1]);
 
-arrayColores.set(1,'#ff6961');
-arrayColores.set(2,'#08cad1');
-arrayColores.set(3,'#f8f38d');
-arrayColores.set(4,'#42d6a4');
-arrayColores.set(5,'#9d94ff');
-arrayColores.set(6,'#ffb480');
-arrayColores.set(7,'#c780e8');
-arrayColores.set(8,'#59adf6');
+mapColores.set(1,'#ff6961');
+mapColores.set(2,'#08cad1');
+mapColores.set(3,'#f8f38d');
+mapColores.set(4,'#42d6a4');
+mapColores.set(5,'#9d94ff');
+mapColores.set(6,'#ffb480');
+mapColores.set(7,'#c780e8');
+mapColores.set(8,'#59adf6');
+
+crearOpciones();
+crearFilaPartida();
+
+
+function crearOpciones(){
+    let contenedorOpciones = document.getElementById("botonesOpciones");
+    mapColores.forEach(function(value, key){
+        let botonOpcion = document.createElement("button");
+        botonOpcion.style.backgroundColor = value;
+        botonOpcion.value=key;
+        botonOpcion.style.width='40px';
+        botonOpcion.style.height='40px';
+        botonOpcion.style.borderRadius="20px";
+        
+        botonOpcion.addEventListener('click',function(){
+            botonOpcion.classList.add("activo");
+            console.log(key);
+            colorPintar=key;
+        });    
+        contenedorOpciones.appendChild(botonOpcion);
+        console.log(key)
+        console.log(value)
+    });
+    
+}
 
 //Crear combinación correcta para adivinar
 function crearCombinacion(){
@@ -31,10 +59,51 @@ function crearCombinacion(){
 }
 
 function crearFilaPartida(){
-
+    let idBoton = 1;
+    let contenedor = document.getElementById("contenedor");
+    //Contenedor de los botones
+    let contenedorBotones = document.createElement("div");
+    contenedorBotones.classList.add("contenedorBotones");
+    //botones del contenedor, dependenderá del nivel
+    for(let i = 0; i < nivel; i++){
+        console.log("Entra");
+        let boton=document.createElement("button");
+        boton.id=`btn${idBoton}`;
+        idBoton++;
+        boton.style.backgroundColor = "white";
+        boton.style.width='40px';
+        boton.style.height='40px';
+        boton.style.borderRadius="20px";
+        boton.addEventListener('click',function(){
+            if(colorPintar != 0){
+                boton.style.backgroundColor = mapColores.get(colorPintar);
+                boton.value=colorPintar;
+                colorPintar=0;
+                //Quitar activo
+                var botonesIterar = document.getElementById('botonesOpciones').getElementsByTagName('button');
+                for (var i = 0; i < botonesIterar.length; i++) {
+                botonesIterar[i].classList.remove('activo');
+                }
+            }
+        });    
+        contenedorBotones.appendChild(boton);
+    }
+    let botonComprobar = document.createElement("button");
+    botonComprobar.innerHTML="Comprobar"
+    botonComprobar.addEventListener('click',function(){
+        //Recorrer los botones para obtener sus valores
+        var botonesIterar2 = document.getElementsByClassName('contenedorBotones');
+            for (var i = 0; i < botonesIterar2.length; i++) {
+                console.log(botonesIterar2[i].value)
+            }
+    });
+    contenedor.appendChild(contenedorBotones);
+    contenedor.appendChild(botonComprobar)
 }
 
+
 function comprobarIntento(arrayTirada){
+    tiradaAciertos=[];
     numIntento++;
     intentos.set(numIntento,arrayTirada);
     console.log('Numero intento ' + intentos.size);
@@ -43,14 +112,18 @@ function comprobarIntento(arrayTirada){
             console.log('Está en la combiacion')
             if(combinacion[i] === arrayTirada[i]){
                 console.log('Y además has acertado')
+                tiradaAciertos.push(1);
+            }else {
+                tiradaAciertos.push(2);
             }
         }else {
             console.log('No está en la combinación')
+            tiradaAciertos.push(3);
         }  
     }
 
+    for (let i = 0; i < tiradaAciertos.length; i++) {
+        console.log('Tirada intento ' + tiradaAciertos[i]);
+    }
 
-    arrayTirada.forEach(function(element) {
-        
-    });
 }
